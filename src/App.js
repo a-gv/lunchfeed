5ab38592d92bfbb5d3e41fa3463ab35410b18c23
a-gv <adagarvelink@me.com>
2020-02-24 18:08:00 +0100

    first project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import Title from './components/Title'
 import './App.css';
+import Feed from './components/Feed'
+
+const dish = [{
+  dishName:"salad",
+  dishPicture:"img",
+  ingredients:"salad with bulghur",
+  like: true,
+  comment:"looks nice"
+}]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <title>Lunchfeed</title>
+  <Title />
+  <Feed />
+
     </div>
-  );
-}
+
+
+    )}
+
 
 export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,6 @@
+import React from "react";
+import '../App.css'
+
+export default function Title(){
+  return <h1>What is for lunch today?</h1>
+}
